5d6f562d6770494a05e1447abf3e12aba6e4d4e2
dariaman <dariaman.siagian@gmail.com>
2017-08-16 18:36:30 +0700

    asd

--- src/main/java/sample/config/WebInitializer.java
+++ src/main/java/sample/config/WebInitializer.java
@@ -20,7 +20,7 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;
     }
 
     @Override
